5bc36ba4d872c28f6ebbe15d4d2d92e48d87967f
abeyuyajp <yellowmonkey0618@gmail.com>
2021-02-17 16:58:44 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,13 +5,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :delivery_fee
   belongs_to :prefecture
+  belongs_to :delivery_fee
   belongs_to :shipping_day
 
   with_options presence: true do
     validates :name
     validates :text
+    validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :condition_id
@@ -19,8 +20,6 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :shipping_day_id
     end
-    validates :price, format: {with:/\A[0-9]+\z/}
-    #「販売価格は¥300~¥9,999,999の間のみ保存可能であること」に対する
-    #バリデーションを記述
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end
