0da33344b6e978a1830da157b9b87aeb1d3fd2e1
abeyuyajp <yellowmonkey0618@gmail.com>
2021-02-23 14:25:28 +0900

    Payjpによるクレジットカード機能の導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,11 +3,13 @@ class PurchasesController < ApplicationController
 
   def index
     @purchase_delivery_address = PurchaseDeliveryAddress.new
+    @purchase_delivery_addresses = Item.find(params[:item_id])
   end
   
   def create
     @purchase_delivery_address = PurchaseDeliveryAddress.new(purchase_params)
     if @purchase_delivery_address.valid?
+      pay_item
       @purchase_delivery_address.save
       redirect_to root_path
     else
@@ -17,6 +19,15 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_delivery_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_delivery_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Item.find(params[:item_id]).price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_delivery_address.rb
+++ app/models/purchase_delivery_address.rb
@@ -1,7 +1,7 @@
 class PurchaseDeliveryAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -9,6 +9,7 @@ class PurchaseDeliveryAddress
     validates :house_number
     validates :phone_number, length:{maximum:11}, format:{with: /\A[0-9]\d+\z/}
     validates :user_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
